70333eca9208ccc76f6db749bb29ddfc35965bd6
Tarek Kharma <tarek95ramos@gmail.com>
2024-01-17 02:49:16 +0100

    set-up-authentication

---
+++ src/pages/Authentication.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect } from "react";
+import { Logo } from "../assets";
+import { Footer } from "../containers";
+import { AuthButtonWithProvider, MainSpinner } from "../components";
+import { FaGoogle, FaGithub } from "react-icons/fa6";
+import useUser from "../hooks/useUser";
+import { useNavigate } from "react-router-dom";
+
+const Authentication = () => {
+  const { data, isLoading, isError } = useUser();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isLoading && data) {
+      navigate("/", { replace: true });
+    }
+  }, [isLoading, data, navigate]);
+
+  if (isLoading) {
+    return <MainSpinner />;
+  }
+
+  return (
+    <div className="auth-section">
+      <img src={Logo} className="w-12 h-auto object-contain" alt="" />
+      <div className="w-full flex flex-1 flex-col items-center justify-center gap-6">
+        <h1 className="text-3xl lg:text-4xl text-blue-700">
+          Welcome to CVHubb
+        </h1>
+        <p className="text-base text-gray-600">
+          shape your resume, craft your success
+        </p>
+        <h2 className="text-2xl text-gray-600">Authenticate</h2>
+        <div className="w-full lg:w-96 p-2 rounded-md flex flex-col items-center justify-start gap-6">
+          <AuthButtonWithProvider
+            Icon={FaGoogle}
+            label={"Sign in with Google"}
+            provider={"GoogleAuthProvider"}
+          />
+          <AuthButtonWithProvider
+            Icon={FaGithub}
+            label={"Sign in with GitHub"}
+            provider={"GithubAuthProvider"}
+          />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Authentication;
---
+++ src/pages/HomeScreen.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomeScreen = () => {
+  return <div className="">HomeScreen</div>;
+};
+
+export default HomeScreen;
