87101a154f2a173f582400cadcddc4a4f3e3bf32
Tarek Kharma <tarek95ramos@gmail.com>
2024-06-04 15:33:55 +0200

    Setup template display page
    - added Edit Template section
    - added suggested templates

--- src/pages/TemplateDesignPinDetails.jsx
+++ src/pages/TemplateDesignPinDetails.jsx
@@ -6,7 +6,7 @@ import {
   saveToCollections,
   saveToFavourites,
 } from "../api";
-import { MainSpinner } from "../components";
+import { MainSpinner, TemplateDesignPin } from "../components";
 import { FaHouse } from "react-icons/fa6";
 import {
   BiFolderPlus,
@@ -16,6 +16,7 @@ import {
 } from "react-icons/bi";
 import useUser from "../hooks/useUser";
 import useTemplates from "../hooks/useTemplates";
+import { AnimatePresence } from "framer-motion";
 
 const TemplateDesignPinDetails = () => {
   const { templateID } = useParams();
@@ -28,7 +29,7 @@ const TemplateDesignPinDetails = () => {
   const { data: user, refetch: userRefetch } = useUser();
 
   const {
-    data: template,
+    data: templates,
     refetch: temp_refetch,
     isLoading: temp_isLoading,
   } = useTemplates();
@@ -163,8 +164,74 @@ const TemplateDesignPinDetails = () => {
         </div>
 
         {/**right section */}
-        <div className="col-span-1 lg:col-span-4">2</div>
+        <div className="col-span-1 lg:col-span-4 w-full flex flex-col items-center justify-start px-3 gap-6">
+          {/**discover  more option */}
+          <div
+            className="w-full h-72 bg-blue-200 rounded-md overflow-hidden relative"
+            style={{
+              background:
+                "url(https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg)",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+            }}
+          >
+            <div className="absolute inset-0 flex items-center justify-center bg-[rgba(0,0,0,0.4)]">
+              <Link
+                to={"/"}
+                className="px-4 py-2 rounded-md border-2 border-gray-50 text-white"
+              >
+                Dsicover More
+              </Link>
+            </div>
+          </div>
+          {/**edit the template */}
+          {user && (
+            <Link
+              className="w-full py-3 px-4 rounded-md flex items-center justify-center bg-emerald-500 cursor-pointer"
+              to={`/resume/${data?.name}?templateId=${templateID}`}
+            >
+              <p className="text-white font-semibold text-lg">
+                Edit This Template
+              </p>
+            </Link>
+          )}
+          {/**tags */}
+          <div className="w-full flex items-center justify-start flex-wrap gap-2 ">
+            {data?.tags?.map((tag, index) => (
+              <p
+                key={index}
+                className="text-xs border border-gray-300 px-2 py-1 rounded-md whitespace-nowrap"
+              >
+                {tag}
+              </p>
+            ))}
+          </div>
+        </div>
       </div>
+      {/**similar templates */}
+      {templates?.filter((temp) => temp._id !== data?._id)?.length > 0 && (
+        <div className="w-full py-8 flex flex-col items-start justify-start gap-4">
+          <p className="text-lg font-semibold text-txtDark">
+            You might also like
+          </p>
+          <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-2">
+            <React.Fragment>
+              <AnimatePresence>
+                {templates &&
+                  templates
+                    ?.filter((temp) => temp._id !== data?._id)
+                    .map((template, index) => (
+                      <TemplateDesignPin
+                        key={template?._id}
+                        data={template}
+                        index={index}
+                      />
+                    ))}
+              </AnimatePresence>
+            </React.Fragment>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
